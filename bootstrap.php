266af93fb6dcc28e5775c0c5d529302cff0127ca
Benjamin Eberlei <kontakt@beberlei.de>
2013-03-17 12:44:46 +0100

    Moved code to src/ instead of entities/

--- bootstrap.php
+++ bootstrap.php
@@ -5,7 +5,7 @@ require_once "vendor/autoload.php";
 
 // Create a simple "default" Doctrine ORM configuration for XML Mapping
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/entities"), $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/src"), $isDevMode);
 // or if you prefer yaml or annotations
 //$config = Setup::createXMLMetadataConfiguration(array(__DIR__."/config/xml"), $isDevMode);
 //$config = Setup::createYAMLMetadataConfiguration(array(__DIR__."/config/yaml"), $isDevMode);
