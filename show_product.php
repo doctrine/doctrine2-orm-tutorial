4ea14ae221734cba3b6d6023af86049998218b31
Benjamin Eberlei <kontakt@beberlei.de>
2013-04-08 23:34:06 +0200

    Add missing scripts

---
+++ show_product.php
@@ -0,0 +1,13 @@
+<?php
+// show_product.php <id>
+require_once "bootstrap.php";
+
+$id = $argv[1];
+$product = $entityManager->find('Product', $id);
+
+if ($product === null) {
+    echo "No product found.\n";
+    exit(1);
+}
+
+echo sprintf("-%s\n", $product->getName());
---
+++ update_product.php
@@ -0,0 +1,17 @@
+<?php
+// update_product.php <id> <new-name>
+require_once "bootstrap.php";
+
+$id = $argv[1];
+$newName = $argv[2];
+
+$product = $entityManager->find('Product', $id);
+
+if ($product === null) {
+    echo "Product $id does not exist.\n";
+    exit(1);
+}
+
+$product->setName($newName);
+
+$entityManager->flush();
