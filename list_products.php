6d1445b08f4db66e7d118da57674dfc5af3cea9c
Jachim Coudenys <jachim.coudenys@combellgroup.com>
2018-01-23 13:05:34 +0100

    Add the missing list_products.php file

---
+++ list_products.php
@@ -0,0 +1,10 @@
+<?php
+// list_products.php
+require_once "bootstrap.php";
+
+$productRepository = $entityManager->getRepository('Product');
+$products = $productRepository->findAll();
+
+foreach ($products as $product) {
+    echo sprintf("-%s\n", $product->getName());
+}
